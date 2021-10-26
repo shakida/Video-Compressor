aac809551b8f4a3c876d01a6c0149730015d3681
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-10-26 15:43:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+
+bot = Client(
+    ":memory:",
+    '2177518',
+    '6f700167aeed1f5d546eab443e85bd7d',
+    bot_token='1851824879:AAHu_kSVPmJusQOVGE9y_f7RLcMoym_jgwg')
+bot.start()
