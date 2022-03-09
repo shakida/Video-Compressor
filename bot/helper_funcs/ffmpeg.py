5e71dd40f7ddd948ae2bc8335453efc5689bc890
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2022-03-09 16:30:09 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -26,11 +26,11 @@ from bot import (
     DOWNLOAD_LOCATION
 )
 
-async def convert_video(saved_file_path, DOWNLOAD_LOCATION, duration, bot, sent_message, target_crf, isAuto, compress_start):
+async def convert_video(video, DOWNLOAD_LOCATION, duration, bot, sent_message, target_crf, isAuto, compress_start):
     # https://stackoverflow.com/a/13891070/4723940
     LOGGER.info('Convert started')
     output_directory = DOWNLOAD_LOCATION
-    video_file = saved_file_path
+    video_file = video
     message = sent_message
     out_put_file_name = output_directory + \
         "/" + str(round(time.time())) + ".mp4"
